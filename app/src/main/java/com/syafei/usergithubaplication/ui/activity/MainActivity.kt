7f048c3d928c75d28196a5e3892001a1a7c6c4a4
zhavei <zhavei.vei@gmail.com>
2022-07-06 16:24:07 +0700

    main

--- app/src/main/java/com/syafei/usergithubaplication/ui/activity/MainActivity.kt
+++ app/src/main/java/com/syafei/usergithubaplication/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.syafei.usergithubaplication
+package com.syafei.usergithubaplication.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.syafei.usergithubaplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
